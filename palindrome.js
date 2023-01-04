d130d2a5c9835633edb04c8df4a7a085bdc35918
Juned Shaikh <junedjshaikh9145@gmail.com>
2023-01-04 18:33:29 +0530

    i have some issue regarding my code please help me

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+let a=aba
+let b=""
+for(let i=a.length;i=>0;i--)
+{
+	b=b+a[i]
+}
+
+if(a==b)
+{
+	console.log("palindrome");
+}
+else
+{
+	console.log("Not palindrome");
+}
---
+++ prime.js
@@ -0,0 +1,17 @@
+let n=13
+let count=0
+for(let i=1;i<=n;i++)
+{
+	if(n%i==0)
+	{
+		count++
+	}
+}
+	if(count==1)
+{
+	console.log("prime")
+}
+else
+{
+console.log("not prime"
+}
